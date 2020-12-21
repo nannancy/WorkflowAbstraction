2323730e7c508b51854d2a9f5372d9f87350d0a0
nannancy <913052176@qq.com>
2020-12-21 11:03:24 +0100

    21 Dec Commit Completed Project for Master thesis

---
+++ src/main/java/interfaces/NodeInterface.java
@@ -0,0 +1,57 @@
+package interfaces;
+
+import java.util.Collection;
+import java.util.Set;
+
+import IOHelper.OntologyHelper;
+
+public interface NodeInterface{
+	/*
+	 * what does a node contain?
+	 * a function returning its semantic description
+	 * a function returning its preceding node
+	 * a function returning its preceding edge
+	 * a function returning its type
+	 * a function returning its actor
+	 * a function returning its id
+	 * 
+	 * a function returning true if it is a start event
+	 * a function returning true if it is an End event
+	 * a function returning true if it is a JOIN node
+	 * a function returning true if it is a SPLIT node
+	 */
+	
+	public String getSemanticDescription();
+	public NodeInterface getPrecedingNode();
+	public NodeInterface getSucceedingNode();
+	public Set<NodeInterface> getAllSucceedingNodes();
+	public Set<NodeInterface> getAllPrecedingNodes();
+	public String getType();
+	public String getActor();
+	public String getId();
+	public String getFormatedId();
+	
+	public void setSemanticDescription(String semanticDescription);
+	public void setActor(String actor);
+	public void setId(String id);
+
+	public boolean isStartEvent();
+	public boolean isEndEvent();
+	public boolean isTask();
+	public boolean isIntermediateEvent();
+	public boolean isJOINNode();
+	public boolean isSPLITNode();
+	public boolean isAbstractNode();
+	
+	public void setSucceedingNode(NodeInterface succeedingNode);
+	public void setPrecedingNode(NodeInterface precedingNode);
+	public void setType(String type);
+	public void addIntoSucceedingNode(NodeInterface node);
+	public void addIntoPrecedingNodes(NodeInterface node);
+	public void addAllIntoParticipants(Set<String> actors);
+	
+	 @Override
+     public int hashCode();
+     @Override
+     public boolean equals(Object obj);
+}
